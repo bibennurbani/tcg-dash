fb483aa2c6d1646d9c6814719d89d301e0c74097
Biben Nurbani Hasan <bibennurbani@gmail.com>
2024-11-09 20:04:03 +0700

    Create Invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,32 @@
+'use server';
+
+import { Prisma, PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const prisma = new PrismaClient();
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date();
+  await prisma.invoice.create({
+    data: {
+      amount: amountInCents,
+      date: date,
+      status: status,
+      customerId: customerId,
+    },
+  });
+}
